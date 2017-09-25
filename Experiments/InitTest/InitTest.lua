84857a4000213f5c5de98d40d83dd4cbbb67b1a3
Alex <metalgear753@gmail.com>
2017-09-25 19:54:33 -0400

    Added InitialTest.
    This is a test plugin, completely useless, but shows the basics.

---
+++ Experiments/InitTest/InitTest.lua
@@ -0,0 +1,30 @@
+local EventFrame = CreateFrame("Frame")
+EventFrame:RegisterEvent("PLAYER_LOGIN")
+EventFrame:SetScript("OnEvent", function(self,event,...) 
+	if type(options) ~= "table" then -- Initialize.
+		options = {}
+		options[UnitName("Player")] = 1
+		ChatFrame1:AddMessage('Welcome, '.. UnitName("Player") ..'. Add-on initialized.')
+	else
+		if options[UnitName("Player")] == 1 then
+			ChatFrame1:AddMessage('Welcome, '.. UnitName("Player") ..'. Your visit count is '.. options[UnitName('Player')] ..' time.')
+		else if options[UnitName("Player")] > 1 then
+			ChatFrame1:AddMessage('Welcome, '.. UnitName("Player") ..'. Your visit count is '.. options[UnitName('Player')] ..' times.')
+		end
+		local found = 0
+                for name,number in pairs(options) do 
+                        if UnitName("Player") == name then
+                                options[name] = options[name] + 1
+                                found = 1
+                        end
+                end
+                if found == 0 then
+						ChatFrame1:AddMessage('Welcome, '.. UnitName("Player") ..'. This is your first visit!')
+                        options[UnitName("Player")] = 1
+                end
+	end
+end)
+
+function Greeting()
+	print("Greetings".. UnitName("Player") .."!")
+end
